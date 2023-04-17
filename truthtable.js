dd3807a25c4c8e093aeb050c4327b0c1f73cc7d9
Eti-ini Effiong-Robert <etinieffiongrobert@gmail.com>
2023-04-17 18:51:48 +0100

    Implemented variable finder

---
+++ truthtable.js
@@ -0,0 +1,26 @@
+const OPERATORS = ["∧", "~", "∨", "→", "↔", "(", ")"];
+let inputForm = {};
+window.addEventListener("load", () => {
+    inputForm = document.getElementById("logic-input");
+})
+
+function addSymbol(char) {
+    inputForm.value += char;
+    inputForm.focus();
+};
+
+function getTokens(string) {
+    let variables = [];
+    for (let char of string) {
+        if (!OPERATORS.includes(char) && char !== " " && !variables.includes(char)) {
+            variables.push(char);
+        };
+    };
+    return variables;
+};
+
+function evaluateLogicString(event) {
+    let logicString = inputForm.value.toLowerCase();
+    let tokens = getTokens(logicString);
+    console.log(variables);
+}
