d232a4395d4c2381e3350cd56a4b4fe302aef46b
Eti-ini Effiong-Robert <etinieffiongrobert@gmail.com>
2023-04-17 21:45:28 +0100

    CSS improvements; logic work

--- truthtable.js
+++ truthtable.js
@@ -9,6 +9,36 @@ function addSymbol(char) {
     inputForm.focus();
 };
 
+function doLogic(a, b, operator) {
+    if (operator === "OR") {
+        return (a || b);
+    } else if (operator === "AND") {
+        return (a && b);
+    } else if (operator === "CONDITIONAL") {
+        // only false when T -> F
+        if (a === 1 && b === 0) {
+            return 0;
+        } else {
+            return 1;
+        }
+    } else if (operator === "BICONDITIONAL") {
+        // T when both are the same, false otherwise
+        if (a == b) {
+            return 1;
+        } else {
+            return 0;
+        }
+    }
+};
+
+function evaluate(string) {
+    if (string.length == 1) {
+        return Number(string);
+    } else {
+        ;
+    }
+}
+
 function getTokens(string) {
     let variables = [];
     for (let char of string) {
