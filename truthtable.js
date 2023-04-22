8e7d5b8f946a261239e14c049bda5241f4771257
Eti-ini Effiong-Robert <etinieffiongrobert@gmail.com>
2023-04-22 19:44:52 +0100

    Table design

--- truthtable.js
+++ truthtable.js
@@ -1,4 +1,4 @@
-const OPERATORS = ["∧", "~", "∨", "→", "↔", "(", ")"];
+const OPERATORS = ["∧", "~", "∨", "→", "↔"];
 const NEGATION = "~";
 const CONJUNCTION = "∧";
 const DISJUNCTION = "∨";
@@ -6,23 +6,34 @@ const IMPLICATION = "→";
 const BICONDITIONAL = "↔";
 
 let inputForm = {};
+let logicOperatorContainer = {}
+
 window.addEventListener("load", () => {
+    // initializes the app
     inputForm = document.getElementById("logic-input");
     inputForm.addEventListener("change", () => {
         inputForm.value = inputForm.value.toLowerCase();
         console.log("Changed!")
     });
+    logicOperatorContainer = document.getElementById("logic-operator-container");
 })
+
 function addSymbol(char) {
+    // adds a logical symbol to the input form
     inputForm.value += char;
     inputForm.focus();
 };
 
-function enteredLogic(element, event) {
+function makeLowercase(element, event) {
+    // converts everything inn the imput form to lowercase
     element.value = element.value.toLowerCase();
 }
 
 function doLogic(a, b, operator) {
+    /* 
+        This function works when A and B are in 1s and 0s. 
+        It evaluates the logic for every symbol except NOT
+    */
     if (operator === "OR") {
         return (a || b);
     } else if (operator === "AND") {
@@ -44,13 +55,8 @@ function doLogic(a, b, operator) {
     }
 };
 
-function simplify(string) {
-    /* 
-        This function will split the string based on parentheses
-    */
-}
-
-function getTokens(string) {
+function getLogicVariables(string) {
+    // gets all the logical variables in the string
     let variables = [];
     for (let char of string) {
         if (!OPERATORS.includes(char) && char !== " " && !variables.includes(char)) {
@@ -60,8 +66,12 @@ function getTokens(string) {
     return variables;
 };
 
+logicOperatorContainer.style.display = "None";
 function evaluateLogicString(event) {
-    let logicString = inputForm.value.toLowerCase();
-    let variables = getTokens(logicString);
+    // The main function of the program
+    let logicString = inputForm.value
+    let variables = getLogicVariables(logicString);
     console.log(variables);
+    let simplified = simplify(logicString);
+    console.log(simplified);
 }
